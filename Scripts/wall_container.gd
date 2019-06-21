4f0a72cdeaac1c414dd317a382a173a29eea7605
cjaws <cjaworsk@gmail.com>
2019-06-21 13:51:49 -0700

    update from pc

--- Scripts/wall_container.gd
+++ Scripts/wall_container.gd
@@ -8,23 +8,23 @@ func _ready():
 
 func spawn_wall():
 	#spawn down from top left
-	for i in range(34):
+	for i in range(64):
 		var wall = scn_wall.instance()
 		add_child(wall)
-		wall.position.x = 13
-		wall.position.y = 25*i + 60
+		wall.position.x = 10
+		wall.position.y = 20*i + 60
 			
 	#spawn right from top left
-	for i in range(40):
+	for i in range(38):
 		var wall = scn_wall.instance()
 		add_child(wall)
-		wall.position.x = 25*i +13
+		wall.position.x = 20*i +10
 		wall.position.y = 60
 
 	#spawn down from top right
-	for i in range(34):
+	for i in range(64):
 		var wall = scn_wall.instance()
 		add_child(wall)
-		wall.position.x = 1000-13
-		wall.position.y = 26*i + 60
+		wall.position.x = 750-10
+		wall.position.y = 20*i + 60
 	pass
